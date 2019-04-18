fdd7997094ede36e7de91aeeac8dfc3aa0b05a8f
rigma <romain@ornikar.com>
2019-04-18 13:23:08 +0200

    feat: adding base rope data structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,29 @@
+pub struct Rope {
+    pub length: usize,
+    fragment: Option<String>
+}
+
+impl Rope {
+    pub fn new(raw: &str) -> Self {
+        Rope {
+            length: raw.len(),
+            fragment: Some(String::from(raw)),
+        }
+    }
+
+    pub fn is_leaf(&self) -> bool {
+        self.fragment.is_some()
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn it_allocates_a_new_rope() {
+        let rope = Rope::new("hello world");
+
+        assert!(rope.is_leaf());
     }
 }
