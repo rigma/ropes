bd1a2c9d45d4daeee5c587118d79ec46dd8d5b35
rigma <rigbuntu@gmail.com>
2019-04-19 23:24:03 +0200

    feat: adding tree balancing method

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,79 @@ impl Rope {
         self.fragment.is_some()
     }
 
+    pub fn get_left_right_ratio(&self) -> f32 {
+        if self.is_leaf() {
+            return std::f32::NAN;
+        }
+
+        let children_lengths = self.get_children_lengths();
+        if children_lengths.1 > 0 {
+            children_lengths.0 as f32 / children_lengths.1 as f32
+        } else {
+            std::f32::NAN
+        }
+    }
+
+    pub fn get_right_left_ratio(&self) -> f32 {
+        if self.is_leaf() {
+            return std::f32::NAN;
+        }
+
+        let children_lengths = self.get_children_lengths();
+        if children_lengths.0 > 0 {
+            children_lengths.1 as f32 / children_lengths.0 as f32
+        } else {
+            std::f32::NAN
+        }
+    }
+
+    pub fn build_tree(&mut self) {
+        if self.is_leaf() {
+            return;
+        }
+
+        self.fragment = Some(format!(
+            "{}{}",
+            (*self.left.take().unwrap()).to_string(),
+            (*self.right.take().unwrap()).to_string(),
+        ));
+
+        self.adjust();
+    }
+
+    pub fn balance(&mut self) -> Result<(), ()> {
+        if self.is_leaf() {
+            return Err(());
+        }
+
+        let ratios = (
+            self.get_left_right_ratio(),
+            self.get_right_left_ratio()
+        );
+
+        if ratios.0.is_nan()
+            || ratios.0 >= REBALANCE_RATIO
+            || ratios.1.is_nan()
+            || ratios.1 >= REBALANCE_RATIO
+        {
+            self.build_tree();
+        } else {
+            if let Some(ref mut child) = self.left {
+                child.balance()?;
+            } else {
+                return Err(());
+            }
+
+            if let Some(ref mut child) = self.right {
+                child.balance()?;
+            } else {
+                return Err(());
+            }
+        }
+
+        Ok(())
+    }
+
     pub fn to_string(&self) -> String {
         if self.is_leaf() {
             if let Some(fragment) = &self.fragment {
@@ -34,13 +107,15 @@ impl Rope {
                 String::new()
             }
         } else {
-            let left = match &self.left {
-                Some(child) => (*child).to_string(),
-                None => String::new(),
+            let left = if let Some(ref child) = self.left {
+                child.to_string()
+            } else {
+                String::new()
             };
-            let right = match &self.right {
-                Some(child) => (*child).to_string(),
-                None => String::new(),
+            let right = if let Some(ref child) = self.right {
+                child.to_string()
+            } else {
+                String::new()
             };
 
             String::from(format!("{}{}", left, right))
@@ -67,6 +142,21 @@ impl Rope {
                 ))
             }
         }
+    }
+
+    fn get_children_lengths(&self) -> (usize, usize) {
+        (
+            if let Some(ref child) = self.left {
+                child.length
+            } else {
+                0
+            },
+            if let Some(ref child) = self.right {
+                child.length
+            } else {
+                0
+            }
+        )
     }
 }
 
